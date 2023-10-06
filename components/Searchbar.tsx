6a1cbddb3fb7c9506340619e0d2a54c40d386747
darshil89 <darshilmahraur67@gmail.com>
2023-10-06 10:51:38 +0530

    search bar is ready to show the scrapp products

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -21,10 +21,20 @@ const isValidAmazonProduct = (url: string) => {
 
 const Searchbar = () => {
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const isValidLink = isValidAmazonProduct(search);
     alert(isValidLink ? "Valid Link" : "Invalid Link");
+
+    try {
+      setLoading(true);
+      //Scrap the product details
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
@@ -35,15 +45,14 @@ const Searchbar = () => {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button type="submit" className="searchbar-btn" disabled={search === ""}>
+        {loading ? "Searching..." : "Search"}
       </button>
     </form>
   );
 };
 
 export default Searchbar;
-
 
 // notes
 // const parseUrl = new URL(url);: This line of code creates a new URL object by calling the URL constructor and passing a URL string (url) as an argument. The URL constructor is a built-in JavaScript object that allows you to work with URLs easily. It parses the provided URL string and creates a URL object with various properties representing different parts of the URL.
